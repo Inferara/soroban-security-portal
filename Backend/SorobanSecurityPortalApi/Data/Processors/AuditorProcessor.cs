954107272a47c69ca51b8818e195597593f301df
Georgii Plotnikov <accembler@gmail.com>
2025-08-26 14:01:30 +0900

    Add PR build workflow

--- Backend/SorobanSecurityPortalApi/Data/Processors/AuditorProcessor.cs
+++ Backend/SorobanSecurityPortalApi/Data/Processors/AuditorProcessor.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using SorobanSecurityPortalApi.Common.Data;
 using SorobanSecurityPortalApi.Models.DbModels;
+using SorobanSecurityPortalApi.Models.ViewModels;
 
 namespace SorobanSecurityPortalApi.Data.Processors
 {
