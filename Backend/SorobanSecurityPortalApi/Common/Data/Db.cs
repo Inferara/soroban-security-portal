a4daac01d6c17e2df0b6fea640d9b47df7b5f495
pope-h <hamatthan@gmail.com>
2026-01-24 20:28:21 +0100

    updated the Db data

--- Backend/SorobanSecurityPortalApi/Common/Data/Db.cs
+++ Backend/SorobanSecurityPortalApi/Common/Data/Db.cs
@@ -20,6 +20,7 @@ namespace SorobanSecurityPortalApi.Common.Data
         public DbSet<FileModel> File { get; set; }
         public DbSet<CompanyModel> Company { get; set; }
         public DbSet<BookmarkModel> Bookmark { get; set; }
+        public DbSet<UserProfileModel> UserProfiles { get; set; }
 
 
         private readonly IDbQuery _dbQuery;
@@ -36,7 +37,7 @@ namespace SorobanSecurityPortalApi.Common.Data
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             /* 
-            After updating to EF Core version: 9.0.0, the error “The model for context ‘Db’ has pending changes.” occurs.
+            After updating to EF Core version: 9.0.0, the error "The model for context 'Db' has pending changes." occurs.
             To avoid this error we suppress the corresponding warning
             Reference: https://github.com/dotnet/efcore/issues/34431
             */
@@ -53,7 +54,18 @@ namespace SorobanSecurityPortalApi.Common.Data
                 .Property(x => x.Embedding)
                 .HasColumnType("vector(3072)");
 
-            base.OnModelCreating(builder); 
+            base.OnModelCreating(builder);
+
+            builder.Entity<UserProfileModel>()
+                .HasOne(up => up.Login)
+                .WithOne(l => l.UserProfile)
+                .HasForeignKey<UserProfileModel>(up => up.LoginId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<UserProfileModel>()
+                .HasIndex(up => up.LoginId)
+                .IsUnique();
+
             builder.HasDbFunction(typeof(TrigramExtensions).GetMethod(nameof(TrigramExtensions.TrigramSimilarity))!)
                 .HasName("similarity"); // PostgreSQL built-in function
             foreach (var entity in builder.Model.GetEntityTypes())
